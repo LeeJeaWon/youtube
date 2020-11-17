6d83803f275286dd16a458034072b2732d5cc27b
LeeJeaWon <73148498+LeeJeaWon@users.noreply.github.com>
2020-11-17 17:05:41 +0900

    #2.6_Middleware까지

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log('hi');
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+app.listen(PORT, handleListening);
+
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+/*
+function handleListening() {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+*/
+
+const handleHome = (req, res) => res.send("hellozzz")
+/*
+function handleHome(req, res) {
+    res.send("hello")
+}
+*/
+
+
+const handleProfile = (req, res) => res.send("Profilzzze")
+/*
+function handleProfile(req, res) {
+    res.send("Profile")
+}
+*/
+
+const betweenHome = (req, res, next) => {
+    console.log('between');
+    next();
+}
+
+app.use(betweenHome)
+
+app.get("/" , handleHome)
+
+app.get("/Profile", handleProfile)
+
