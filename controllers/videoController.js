ce36a0bad56fafe8392c5298a701a329cb40945c
LeeJeaWon <73148498+LeeJeaWon@users.noreply.github.com>
2020-11-23 17:13:05 +0900

    #2.21 완료

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,11 +1,27 @@
-export const home = (req, res) => res.render("home", {pageTitle: "Home"});
-export const search = (req, res) => {
-    const searchingBy = req.query.term;
-    res.render("search" , {pageTitle: "search"});
-}
-export const videos = (req, res) => res.render("videos" , {pageTitle: "videos"});
-export const upload = (req, res) => res.render("upload" , {pageTitle: "upload"});
-export const videoDetail = (req, res) => res.render("videoDetail" , {pageTitle: "videoDetail"});
-export const editVideo = (req, res) => res.render("editVideo" , {pageTitle: "editVideo"});
-export const deleteVideo = (req, res) => res.render("deleteVideo" , {pageTitle: "deleteVideo"});
+import { videos2} from "../db"
 
+export const home = (req,res) => {
+    res.render("home", {pageTitle:"Home", videos2})
+}
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy });
+};
+
+export const videos = (req, res) =>
+  res.render("videos", { pageTitle: "videos" });
+
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
+
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", { pageTitle: "Video Detail" });
+
+export const editVideo = (req, res) =>
+  res.render("editVideo", { pageTitle: "Edit Video" });
+
+export const deleteVideo = (req, res) =>
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
