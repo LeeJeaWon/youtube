3dab183937b32efa25d76e3a91cf440c761332f2
LeeJeaWon <73148498+LeeJeaWon@users.noreply.github.com>
2020-12-06 19:25:37 +0900

    github login 2

--- init.js
+++ init.js
@@ -3,6 +3,8 @@ import "./db"
 import dotenv from "dotenv";
 dotenv.config();
 import "./models/Video";
+import "./models/Comment";
+import "./models/User";
 
 const PORT = process.env.PORT || 4000;
 
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,20 +1,31 @@
 import express from "express";
+import passport from "passport";
 import routes from "../routes";
-import { postJoin, getJoin, getLogin,postLogin, logout } from "../controllers/userController";
+import { postJoin, getJoin, getLogin,postLogin, logout, githubLogin, postGithubLogIn } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
+import { onlyPublic,onlyPrivate  } from "../middlewares";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin);
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
 
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, postLogin);
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
 
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.logout, logout);
+
+globalRouter.get(routes.logout, onlyPrivate, logout);
+
+globalRouter.get(routes.gitHub, githubLogin);
+
+globalRouter.get(
+  routes.githubCallback,
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  postGithubLogIn
+);
 
 
 export default globalRouter;
